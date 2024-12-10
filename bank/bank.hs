7e8b2f3b09da831844202c82515724f4d9525a33
Santhoshkumar Reddy Gajjehalli <sgajjehalli@cs.binghamton.edu>
2024-12-09 23:50:48 -0500

    bank code changed

--- bank/bank.hs
+++ bank/bank.hs
@@ -20,25 +20,28 @@ initBankState = BankState {
 
 -- Deposit operation
 deposit :: Float -> BankOp ()
-deposit amount = modify $ \s -> s { balance = balance s + amount }
+deposit amount = modify $ \s -> s {
+    balance = balance s + amount,
+    overdrawn = balance s + amount < 0  -- Update overdrawn flag
+}
 
 -- Withdraw operation
 withdraw :: Float -> BankOp Float
 withdraw amount = do
     s <- get
-    let newBalance = balance s - amount
-        isOverdrawn = newBalance < 0  -- Determine if overdrawn
-        actualWithdraw = if newBalance < -100 then balance s + 100 else amount
-    put $ s { balance = balance s - actualWithdraw, overdrawn = isOverdrawn }
+    let currentBalance = balance s
+        newBalance = currentBalance - amount
+        actualWithdraw =
+            if newBalance < -100 -- Limit to $100 overdraft
+            then currentBalance + 100
+            else amount
+        isOverdrawn = (currentBalance - actualWithdraw) < 0
+    put $ s { balance = currentBalance - actualWithdraw, overdrawn = isOverdrawn }
     return actualWithdraw
 
 -- Get the current balance
 getBalance :: BankOp Float
 getBalance = gets balance
-
--- Get whether the account is overdrawn
-getOverdrawn :: BankOp Bool
-getOverdrawn = gets overdrawn
 
 -- Run a Bank operation starting with the initial state
 runBankOp :: BankOp a -> a
